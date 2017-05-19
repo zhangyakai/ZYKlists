9e2d97942b5919319df4a9ae46c717f7cd1ffa7a
yuzixun <yzxun@qq.com>
2017-05-19 14:58:51 +0800

    删除多余文件

--- list/ViewController.m
+++ list/ViewController.m
@@ -37,7 +37,7 @@
 
 - (NSArray *)topTitleNames {
     if (_topTitleNames == nil) {
-        _topTitleNames = @[@"选项1",@"选项2",@"选项3",@"选项4",@"选项5",@"选项6",@"选项7",@"选项8",@"选项9",@"选项10"];
+        _topTitleNames = @[@"选项1",@"选项2",@"选项3",@"选项4",@"选项5",@"选项6",@"选项7",@"选项8",@"选项9"];
         
     }
     return _topTitleNames;
